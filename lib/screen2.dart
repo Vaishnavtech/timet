bf2b0aeb29ef43b73d554257c5e571033cd2d310
Vaishnav T P <vaishnavtp.in@gmail.com>
2024-02-07 16:36:10 +0530

    First Major change,code reduced

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,273 +1,74 @@
 import 'package:flutter/material.dart';
-class con extends Container{
+
+class box extends StatelessWidget {
   
+    String boxText;
+    box(this.boxText);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.teal[200],
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child:  Text(boxText),
+    );
+  }
 }
+
 class screenone extends StatelessWidget {
   const screenone({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: SizedBox(height: 600,
+          child: SizedBox(
+            height: 600,
             child: GridView.count(
-                scrollDirection: Axis.horizontal,
-                primary: true,
-                padding: const EdgeInsets.all(8),
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-                crossAxisCount: 5,
-                children: [
-            Container(
+              scrollDirection: Axis.horizontal,
+              primary: true,
               padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: TextField(
-  decoration: InputDecoration(
-    border: OutlineInputBorder(
-      
-    ),
-    hintText: 'Enter a search term',
-  ),
-),
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              crossAxisCount: 5,
+              children: [
+                box("test1"),
+                box("test2"),
+                box("test3"),
+                box("test4"),
+                box("test5"),
+                box("test6"),
+                box("test7"),
+                box("test8"),
+                box("test9"),
+                box("test10"),
+                box("test11"),
+                box("test12"),
+                box("test13"),
+                box("test14"),
+                box("test15"),
+                box("test16"),
+                box("test17"),
+                box("test18"),
+                box("test19"),
+                box("test20"),
+                box("test21"),
+                box("test22"),
+                box("test23"),
+                box("test24"),
+                box("test25"),
+                box("test26"),
+                box("test27"),
+                box("test28"),
+                box("test29"),
+                box("test30"),
+              ],
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text("He'd have you all unravel at the"),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text("He'd have you all unravel at the"),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text("He'd have you all unravel at the"),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution, they...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text("He'd have you all unravel at the"),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              padding: const EdgeInsets.all(8),
-              child: const Text('Heed not the rabble'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Sound of screams but the'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Who scream'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution is coming...'),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.teal[200],
-              ),
-              child: const Text('Revolution, they...'),
-            ),
-                ],
-              ),
           ),
         ));
   }
